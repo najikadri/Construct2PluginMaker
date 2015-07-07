fb67cb771388d48b1df5edd1ea014dabd735dc02
Lightbulb Games Studio <naji-newman-2000@hotmail.com>
2015-07-07 15:25:46 +0300

    Create Param.java

---
+++ com/lbg/c2/Param.java
@@ -0,0 +1,21 @@
+package com.lbg.c2;
+
+public interface Param {
+	
+	public void setLabel(String label);
+	
+	public void setDescription(String description);
+	
+	public void setText(String text);
+	
+	public String getLabel ();
+	
+	public String getDescription ();
+	
+	public String getText();
+	
+	public void setValue(String value);
+	
+	public String getValue();
+
+}
